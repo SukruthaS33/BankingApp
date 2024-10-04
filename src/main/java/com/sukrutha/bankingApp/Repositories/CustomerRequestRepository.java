9b12bee25f551e49e47bd4f8996d800147f35d1c
Sukrutha Shamsundar <Sukrutha2024@gmail.com>
2024-10-04 22:25:05 +0530

    customer new request created

--- src/main/java/com/sukrutha/bankingApp/Repositories/CustomerRequestRepository.java
+++ src/main/java/com/sukrutha/bankingApp/Repositories/CustomerRequestRepository.java
@@ -1,5 +1,20 @@
 package com.sukrutha.bankingApp.Repositories;
 
-public class CustomerRequestRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.sukrutha.bankingApp.entities.CustomerRequest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRequestRepository extends JpaRepository<CustomerRequest, String> {
+
+	@Query("SELECT c FROM CustomerRequest c WHERE c.requestStatus = 'SUBMITTED'")
+	List<CustomerRequest> findAllSubmittedRequests();
 }
--- src/main/java/com/sukrutha/bankingApp/services/CustomerRequestService.java
+++ src/main/java/com/sukrutha/bankingApp/services/CustomerRequestService.java
@@ -1,19 +1,65 @@
 package com.sukrutha.bankingApp.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sukrutha.bankingApp.Repositories.CustomerRepository;
+import com.sukrutha.bankingApp.Repositories.CustomerRequestRepository;
 import com.sukrutha.bankingApp.entities.CustomerRequest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class CustomerRequestService {
 
-	public List<CustomerRequest> getAllRequests() {
-		// TODO Auto-generated method stub
+	@Autowired
+	CustomerRequestRepository customerRequestRepository;
+
+	public List<CustomerRequest> getAllRequestsofAllCustomers() {
+		log.info("CustomerRequestService::getAllRequestsofAllCustomers");
+		List<CustomerRequest> allRequests = new ArrayList<CustomerRequest>();
+		try {
+			allRequests = customerRequestRepository.findAllSubmittedRequests();
+			return allRequests;
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("error in getting all the requests");
+		}
 		return null;
+
 	}
 
 	public CustomerRequest getRequestedItemById(String requestId) {
-		// TODO Auto-generated method stub
+		log.info("CustomerRequestService::getRequestedItemById");
+		try {
+			CustomerRequest customerRequest = customerRequestRepository.findById(requestId)
+					.orElseThrow(() -> new Exception());
+			return customerRequest;
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("unable to fetch the request");
+		}
 		return null;
+
+	}
+
+	public CustomerRequest createNewRequestForCustomer(CustomerRequest request) {
+		log.info("CustomerRequestService::createNewRequestForCustomer");
+		CustomerRequest newRequest = null;
+		try {
+			newRequest = customerRequestRepository.save(request);
+			return newRequest;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("unable to fetch the request");
+		}
+		return newRequest;
+
 	}
 
 }
